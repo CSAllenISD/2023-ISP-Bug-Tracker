a81d90225364965778dc095dd3ad0231079ebc2f
Rafid-A <rafid.ahmed@student.allenisd.org>
2023-01-30 19:38:38 -0600

    Initial MVP

---
+++ src/components/CurrentIssue/CurrentIssue.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./CurrentIssue.css"
+
+function CurrentIssue() {
+  return (
+    <div className="current-issues">
+      <div className="indv-issue">
+      <p className="close-issue">Close Issue</p>
+      <p>Assigned<span>Username</span></p>
+      <p>Priority<span>High</span></p>
+      <p>Description</p>
+      <p className="description-text">Short Description of the Project describing the issue</p>
+      <hr />
+      </div>
+      <div className="indv-issue">
+      <p className="close-issue">Close Issue</p>
+      <p>Assigned<span>Username</span></p>
+      <p>Priority<span>High</span></p>
+      <p>Description</p>
+      <p className="description-text">Short Description of the Project describing the issue</p>
+      <hr />
+      </div>
+      <div className="indv-issue">
+      <p className="close-issue">Close Issue</p>
+      <p>Assigned<span>Username</span></p>
+      <p>Priority<span>High</span></p>
+      <p>Description</p>
+      <p className="description-text">Short Description of the Project describing the issue</p>
+      <hr />
+      </div>
+    </div>
+  );
+}
+
+export default CurrentIssue;
