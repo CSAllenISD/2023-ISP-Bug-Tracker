e91cbd05de72a99ddbd411c3dc61586766b08462
Brook-A-CS <brooka048@gmail.com>
2023-02-19 18:08:18 -0600

    removed console logs and renamed 'events' to 'issues'

--- src/components/AddIssue/AssignList.jsx
+++ src/components/AddIssue/AssignList.jsx
@@ -5,7 +5,6 @@ export default function AssignList({assignees}) {
   if (assignees != null){
     return(
       assignees.map(assigned => {
-        console.log(assigned.name, assigned.id)
         return <Assign key={assigned.id} assingees={assigned} /> 
       })
     )
