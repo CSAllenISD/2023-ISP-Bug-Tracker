596ba032d69f2a82c0c45ec215f71b50c75678b7
Rafid-A <rafid.ahmed@student.allenisd.org>
2023-02-24 18:53:35 -0600

    Adding More Description on Landing page
    Alongside adding more description to the landing page for our product
    I have also added some of the new packages I had downloaded in order to
    build the landing page which is reflected in the package-lock.json however
    some of these packages might not be necessary and I will QA that at a later
    commit.

---
+++ src/components/Home/About.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import AboutBackground from "../../Assets/about-background2.png";
+import AboutBackgroundImage from "../../Assets/viewissue.png";
+
+const About = () => {
+  return (
+    <div className="about-section-container">
+      <div className="about-background-image-container">
+        <img src={AboutBackground} alt="" />
+      </div>
+      <div className="about-section-image-container">
+        <img src={AboutBackgroundImage} alt="" />
+      </div>
+      <div className="about-section-text-container">
+        <p className="primary-subheading">About</p>
+        <h1 className="primary-heading">
+          Planning your own life can be very beneficial
+        </h1>
+        <p className="primary-text">
+          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
+          elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
+        </p>
+        <p className="primary-text">
+          Non tincidunt magna non et elit. Dolor turpis molestie dui magnis
+          facilisis at fringilla quam.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
