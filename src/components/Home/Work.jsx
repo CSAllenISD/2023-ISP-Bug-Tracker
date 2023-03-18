3f57898615d91dc80ea1bb57114c18f4d5bc7e2a
Rafid-A <rafid.ahmed@student.allenisd.org>
2023-03-17 22:02:05 -0500

    Adding Work portion to Landing Page
    This is going to go towards the bottom of the landing page where
    this will show our product and highlight some key features about it.
    This allows a user to see what they are getting into before they start
    to use it. The issue this works towards for is
    https://github.com/CSAllenISD/2023-ISP-Bug-Tracker/issues/21

---
+++ src/components/Home/Work.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Pick from "../../Assets/Sky.png";
+import Choose from "../../Assets/Sky.png";
+import Delivery from "../../Assets/Sky.png";
+
+const Work = () => {
+  const workInfoData = [
+    {
+      image: Pick,
+      title: "Choose What you Want",
+      text: "Lorem ipsum dolor sit amet consectetur. Maecenas orci et sagittis duis elementum interdum facilisi bibendum.",
+    },
+    {
+      image: Choose,
+      title: "Add Them",
+      text: "Lorem ipsum dolor sit amet consectetur. Maecenas orci et ",
+    },
+    {
+      image: Delivery,
+      title: "See your Schedule",
+      text: "Lorem ipsum dolor sit amet consectetur. Maecenas orci et lorem ipsum",
+    },
+  ];
+  return (
+    <div className="work-section-wrapper">
+      <div className="work-section-top">
+        <p className="primary-subheading">Work</p>
+        <h1 className="primary-heading">How It Works</h1>
+        <p className="primary-text">
+          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
+          elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
+        </p>
+      </div>
+      <div className="work-section-bottom">
+        {workInfoData.map((data) => (
+          <div className="work-section-info" key={data.title}>
+            <div className="info-boxes-img-container">
+              <img src={data.image} alt="pic" />
+            </div>
+            <h2>{data.title}</h2>
+            <p>{data.text}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Work;
