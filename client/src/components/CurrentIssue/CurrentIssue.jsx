6b5f25e3f8c712cee27b92224c2d09ab3d643752
Aidan Valva <aidanvalv@gmail.com>
2023-04-21 15:17:16 -0500

    Current Issue Button is now Optimally Fixed
    This contributes to issue - https://github.com/CSAllenISD/2023-ISP-Bug-Tracker/issues/47

--- client/src/components/CurrentIssue/CurrentIssue.jsx
+++ client/src/components/CurrentIssue/CurrentIssue.jsx
@@ -1,43 +1,57 @@
-import React from "react";
-import "./CurrentIssue.css"
-
-function remove() {
-  var element = document.getElementsByClassName("indv-issue")[0];
-  element.remove()
-}
+import React, { useState } from "react";
+import "./CurrentIssue.css";
 
 function CurrentIssue() {
+  const [issues, setIssues] = useState([
+    {
+      id: 1,
+      assigned: "Bob",
+      priority: "High",
+      description: "Short Description of the Project describing the issue",
+    },
+    {
+      id: 2,
+      assigned: "Rose",
+      priority: "High",
+      description: "Short Description of the Project describing the issue",
+    },
+    {
+      id: 3,
+      assigned: "Matt",
+      priority: "High",
+      description: "Short Description of the Project describing the issue",
+    },
+  ]);
+
+  const removeIssue = (index) => {
+    const newIssues = [...issues];
+    newIssues.splice(index, 1);
+    setIssues(newIssues);
+  };
+
   return (
     <div className="App">
-    <h1>Current Issues</h1>
-    <div className="grey-card-container">
-    <div className="current-issues">
-      <div className="indv-issue">
-      <button onClick={remove} className="close-issue">Close Issue</button>
-      <p>Assigned<span>Bob</span></p>
-      <p>Priority<span>High</span></p>
-      <p>Description</p>
-      <p className="description-text">Short Description of the Project describing the issue</p>
-      <hr />
+      <h1>Current Issues</h1>
+      <div className="grey-card-container">
+        <div className="current-issues">
+          {issues.map((issue, index) => (
+            <div key={issue.id} className="indv-issue">
+              <button onClick={() => removeIssue(index)} className="close-issue">
+                Close Issue
+              </button>
+              <p>
+                Assigned<span>{issue.assigned}</span>
+              </p>
+              <p>
+                Priority<span>{issue.priority}</span>
+              </p>
+              <p>Description</p>
+              <p className="description-text">{issue.description}</p>
+              <hr />
+            </div>
+          ))}
+        </div>
       </div>
-      <div className="indv-issue">
-      <button onClick={remove} className="close-issue">Close Issue</button>
-      <p>Assigned<span>Rose</span></p>
-      <p>Priority<span>High</span></p>
-      <p>Description</p>
-      <p className="description-text">Short Description of the Project describing the issue</p>
-      <hr />
-      </div>
-      <div className="indv-issue">
-      <button onClick={remove} className="close-issue">Close Issue</button>
-      <p>Assigned<span>Matt</span></p>
-      <p>Priority<span>High</span></p>
-      <p>Description</p>
-      <p className="description-text">Short Description of the Project describing the issue</p>
-      <hr />
-      </div>
-    </div>
-    </div>
     </div>
   );
 }
